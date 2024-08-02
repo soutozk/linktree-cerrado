54571bf56df2d2dfc7beb30084eb2261b0dd971e
soutozk <jgbrmix@gmail.com>
2024-08-02 19:40:52 -0300

    add logo

--- src/script.js
+++ src/script.js
@@ -7,12 +7,11 @@ function toggleMode() {
   //substituir a img
   if (html.classList.contains("light")) {
     //se tiver light mode, add a image light
-    img.setAttribute('src', '/assets/avatar-light.png' )
+    img.setAttribute("src", "/assets/logo-bk.png")
   } else {
-  img.setAttribute("src", "/assets/avatar.png")
+    img.setAttribute("src", "/assets/Design_sem_nome__4_-removebg-preview.png")
   }
-
 
   //se tiver sem light mode, manter a iimagem
 }
-  html.classList.remove("light")
+html.classList.remove("light")
