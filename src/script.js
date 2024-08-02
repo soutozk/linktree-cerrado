85a741ce05cbc9928f0c8d7d6f8403adbcd3eef8
soutozk <jgbrmix@gmail.com>
2024-08-02 16:33:36 -0300

    initial commit

---
+++ src/script.js
@@ -0,0 +1,18 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  //pegar a tag img
+  const img = document.querySelector("#profile img")
+  //substituir a img
+  if (html.classList.contains("light")) {
+    //se tiver light mode, add a image light
+    img.setAttribute('src', '/assets/avatar-light.png' )
+  } else {
+  img.setAttribute("src", "/assets/avatar.png")
+  }
+
+
+  //se tiver sem light mode, manter a iimagem
+}
+  html.classList.remove("light")
